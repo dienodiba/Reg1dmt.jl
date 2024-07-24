95958f88d936d1251d601b681ccacfd2538d077b
Dieno Diba <65894100+dienodiba@users.noreply.github.com>
2024-07-24 13:16:05 +0900

    Create main.jl

---
+++ src/main.jl
@@ -0,0 +1,25 @@
+# Dieno Diba 2022
+# REG1DMT
+# Main program
+
+using DelimitedFiles
+using Statistics
+using SparseArrays
+using LinearAlgebra
+using Printf
+using Dates
+
+function main()
+
+    filedata = readline()
+    filestg = readline()
+    fileout = readline()
+
+    Inv1DMT(filedata,filestg,fileout)
+    
+end
+
+include("Inv1DMT")
+include("Fwd1DMT")
+
+main()
