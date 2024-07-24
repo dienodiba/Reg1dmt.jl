11461ebebd3c4bd5c60dffd4579af2276b137f83
Dieno Diba <65894100+dienodiba@users.noreply.github.com>
2024-07-25 02:56:19 +0900

    Add files via upload

---
+++ src/Reg1dmt.jl
@@ -0,0 +1,17 @@
+# Dieno Diba 2022
+# REG1DMT
+# Main module
+
+module Reg1dmt
+
+using DelimitedFiles
+using Statistics
+using SparseArrays
+using LinearAlgebra
+using Printf
+using Dates
+
+include("Inversion.jl")
+include("ForwardModeling.jl")
+
+end
